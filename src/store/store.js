a35bb7da7220911c7e098906ea8c705477caa0b3
shahsuresh <shahsuresh75@gmail.com>
2024-07-15 14:09:14 +0545

    snackbar slice registered in the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../store/slices/userSlice";
+import snackbarReducer from "../store/slices/snackbarSlice";
 export const reduxStore = configureStore({
   reducer: {
     user: userReducer,
+    snackbar: snackbarReducer,
   },
 });
